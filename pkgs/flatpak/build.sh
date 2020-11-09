1bb1fc76e18202366b26d8f64a469bb9b887f76f
Rafostar <40623528+Rafostar@users.noreply.github.com>
2020-11-09 13:13:09 +0100

    Update flatpak GTK4 to latest git master

--- pkgs/flatpak/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -f com.github.rafostar.Clapper.flatpak
-rm -rf build; mkdir build
-rm -rf repo; mkdir repo
-
-flatpak-builder --ccache --force-clean --default-branch=test build com.github.rafostar.Clapper.yml --repo=repo
