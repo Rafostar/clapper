234a44a34ada2fa88e62397315b512dd1b4eaca5
Rafostar <40623528+Rafostar@users.noreply.github.com>
2021-01-05 22:49:54 +0100

    Use ARGV from main() instead of importing system utils

--- clapper_src/main.js
+++ clapper_src/main.js
@@ -1,13 +1,12 @@
 imports.gi.versions.Gdk = '4.0';
 imports.gi.versions.Gtk = '4.0';
 
-const System = imports.system;
 const { Gst } = imports.gi;
 const { App } = imports.clapper_src.app;
 
 Gst.init(null);
 
-function main()
+function main(argv)
 {
-    new App().run([System.programInvocationName].concat(ARGV));
+    new App().run(argv);
 }
