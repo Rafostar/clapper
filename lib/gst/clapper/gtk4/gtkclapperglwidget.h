06d9f302c2c6649a955fc3601f65d3d06a2e3ca5
Rafał Dzięgiel <rafostar.github@gmail.com>
2021-05-31 18:07:37 +0200

    Revert "sink: Use g_main_context_invoke_full for drawing"
    Not much benefit from using this function and unlike g_idle_add_full it
    does not allow to skip a frame when previous one was not finished (slow HW).
    
    This reverts commit f8a7abe1959946116aa993f34ba892c3da08e6e0.

--- lib/gst/clapper/gtk4/gtkclapperglwidget.h
+++ lib/gst/clapper/gtk4/gtkclapperglwidget.h
@@ -69,7 +69,6 @@ struct _GtkClapperGLWidget
   GstBuffer *pending_buffer;
   GstBuffer *buffer;
   GstVideoInfo v_info;
-  GMainContext *app_context;
 
   /* resize */
   gboolean pending_resize;
@@ -85,6 +84,9 @@ struct _GtkClapperGLWidget
   GtkEventController *key_controller;
   GtkEventController *motion_controller;
   GtkGesture *click_gesture;
+
+  /* Pending draw idles callback */
+  guint draw_id;
 };
 
 struct _GtkClapperGLWidgetClass
