4c0fc1dac2a4de7c206b1b8c00d3dd9e97aad84d
Rafał Dzięgiel <rafostar.github@gmail.com>
2021-08-02 12:28:36 +0200

    Init libadwaita

--- src/mainRemote.js
+++ src/mainRemote.js
@@ -2,6 +2,11 @@ imports.gi.versions.Gdk = '4.0';
 imports.gi.versions.Gtk = '4.0';
 imports.gi.versions.Soup = '2.4';
 
+const { Gtk, Adw } = imports.gi;
+
+Gtk.init();
+Adw.init();
+
 const { AppRemote } = imports.src.appRemote;
 const Misc = imports.src.misc;
 
