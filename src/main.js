3c765e9f178e626da56593a12f94533046da7c9e
Rafostar <40623528+Rafostar@users.noreply.github.com>
2021-09-05 17:03:13 +0200

    Add some init debug messages
    To get an overall idea how long initialization takes

--- src/main.js
+++ src/main.js
@@ -5,14 +5,21 @@ imports.gi.versions.Soup = '2.4';
 pkg.initGettext();
 pkg.initFormat();
 
+const Debug = imports.src.debug;
+Debug.debug('imports');
+
 const { GstClapper, Gtk, Adw } = imports.gi;
 const { App } = imports.src.app;
 
 function main(argv)
 {
+    Debug.debug('main');
+
     GstClapper.Clapper.gst_init(null);
     Gtk.init();
     Adw.init();
+
+    Debug.debug('initialized');
 
     new App().run(argv);
 }
