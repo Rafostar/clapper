e94c3323641ff8080bed1996e05e7b517022cf55
Rafał Dzięgiel <rafostar.github@gmail.com>
2024-01-21 20:51:05 +0100

    doc: Upload initial ClapperGtk API reference

---
+++ doc/clapper-gtk/urlmap.js
@@ -0,0 +1,7 @@
+baseURLs = [
+  ['GLib', 'https://docs.gtk.org/glib/'],
+  ['GObject', 'https://docs.gtk.org/gobject/'],
+  ['Gtk', 'https://docs.gtk.org/gtk4/'],
+  ['Gst', 'https://gstreamer.freedesktop.org/documentation/gstreamer/gi-index.html?'],
+  ['Clapper', 'https://rafostar.github.io/clapper/doc/clapper/'],
+]
