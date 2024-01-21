28fd45a300e47b7b17f0aa0f71d3de0d9f01875d
Rafał Dzięgiel <rafostar.github@gmail.com>
2024-01-21 20:50:31 +0100

    doc: Update Clapper API reference

--- doc/clapper/urlmap.js
+++ doc/clapper/urlmap.js
@@ -1,11 +1,11 @@
-doc_base_urls = [
+baseURLs = [
   ['GLib', 'https://docs.gtk.org/glib/'],
   ['GObject', 'https://docs.gtk.org/gobject/'],
   ['Gio', 'https://docs.gtk.org/gio/'],
   ['Gtk', 'https://docs.gtk.org/gtk4/'],
-  ['Gst', 'https://gstreamer.freedesktop.org/documentation/'],
-  ['GstBase', 'https://gstreamer.freedesktop.org/documentation/base/'],
-  ['GstAudio', 'https://gstreamer.freedesktop.org/documentation/audio/'],
-  ['GstTag', 'https://gstreamer.freedesktop.org/documentation/tag/'],
-  ['GstPbutils', 'https://gstreamer.freedesktop.org/documentation/pbutils/'],
+  ['Gst', 'https://gstreamer.freedesktop.org/documentation/gstreamer/gi-index.html?'],
+  ['GstBase', 'https://gstreamer.freedesktop.org/documentation/base/?'],
+  ['GstAudio', 'https://gstreamer.freedesktop.org/documentation/audio/?'],
+  ['GstTag', 'https://gstreamer.freedesktop.org/documentation/tag/?'],
+  ['GstPbutils', 'https://gstreamer.freedesktop.org/documentation/pbutils/?'],
 ]
